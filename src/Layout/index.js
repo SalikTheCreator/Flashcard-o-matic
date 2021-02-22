8436e9dce824998d117270b4fc3b0e274b8e23e5
Salik Ali <73484829+SalikTheCreator@users.noreply.github.com>
2021-02-22 14:14:15 -0800

    Update index.js

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
 import Home from "./Home";
@@ -6,36 +6,69 @@ import Study from "./Study";
 import NewDeck from "./NewDeck";
 import DeckOverview from "./DeckOverview";
 import EditDeck from "./EditDeck";
-import AddCard from "./AddCard";
+
+import AddCard from "./AddCard"
 import EditCard from "./EditCard";
-import { Route, Switch } from "react-router-dom";
+
+
+import {
+
+  listDecks,
+
+} from "../utils/api/index";
+import {
+  BrowserRouter as Router,
+
+  Route,
+  Switch,
+
+} from "react-router-dom";
 
 function Layout() {
+  const [deckList, setDeckList] = useState([]);
+
+  const handleDeleteDeck = (deckId) => {
+    setDeckList(deckList.filter(deck => deck.id !== deckId));
+  }
+
+  const handleCreateDeck = (deck) => {
+    setDeckList([...deckList, deck]);
+  }
+  
+  useEffect(() => {
+    async function loadDeck() {
+      let allDecks = await listDecks();
+      setDeckList(allDecks);
+    }
+    loadDeck();
+  }, []);
+
   return (
     <div>
       <Header />
       <div className="container">
         <Switch>
           <Route path="/" exact={true}>
-            <Home />
+            <Home handleDeleteDeck={handleDeleteDeck}/>
           </Route>
           <Route path="/decks/new" exact={true}>
-            <NewDeck />
+            <NewDeck handleCreateDeck={handleCreateDeck}/>
           </Route>
           <Route path="/decks/:deckId" exact={true}>
-            <DeckOverview />
+            <DeckOverview deckList={deckList}/>
           </Route>
-          <Route path="/decks/:deckId/edit" exact={true}>
-            <EditDeck />
+
+          <Route path="/decks/:deckId/study">
+            <Study />
+          </Route>
+          <Route path="/decks/:deckId/edit">
+            <EditDeck deckList={deckList}/>
           </Route>
           <Route path="/decks/:deckId/cards/new">
-            <AddCard />
+            <AddCard deckList={deckList}/>
           </Route>
           <Route path="/decks/:deckId/cards/:cardId/edit" exact={true}>
             <EditCard />
-          </Route>
-          <Route path="/decks/:deckId/study">
-            <Study />
           </Route>
           <Route>
             <NotFound />
