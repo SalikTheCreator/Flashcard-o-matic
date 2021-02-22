a41ecb9c8c8e417dd0277b6d94417e18c338bd66
Salik Ali <73484829+SalikTheCreator@users.noreply.github.com>
2021-02-22 14:10:39 -0800

    Update CardList.js

--- src/Layout/CardList.js
+++ src/Layout/CardList.js
@@ -1,29 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import NotFound from "./NotFound";
+import Home from "./Home";
+import Study from "./Study";
+import {
+  stripCards,
+  listDecks,
+  createDeck,
+  readDeck,
+  updateDeck,
+  deleteDeck,
+  listCards,
+  createCard,
+  readCard,
+  updateCard,
+  deleteCard,
+} from "../utils/api/index";
+import {
+  BrowserRouter as Router,
+  Link,
+  NavLink,
+  Route,
+  Switch,
+  useLocation,
+  useHistory,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
 
-function CardList(props) {
-  return (
-    <div class="card">
-      <div class="card-body">
-        <div class="container">
-          <div class="row justify-content-start">
-            <div class="col-4">{props.front}</div>
+function CardList(props){
+    return(
+        <div className="card">
+        <div className="card-body">
+          <div className="container">
+            <div className="row justify-content-start">
+              <div className="col-4">{props.front}</div>
 
-            <div class="col-4">{props.back}</div>
+              <div className="col-4">{props.back}</div>
 
-            <div class="col-4">
-              <Link to="#" class="btn btn-secondary">
-                Edit
-              </Link>
-              <Link to="#" class="btn btn-danger">
-                <span className="oi oi-trash" />
-              </Link>
+              <div className="col-4">
+                <Link to="#" className="btn btn-secondary">
+                  Edit
+                </Link> 
+                <Link to="#" className="btn btn-danger">
+                  <span className="oi oi-trash" />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    )
 }
 
-export default CardList;
+export default CardList
