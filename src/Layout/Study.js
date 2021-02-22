ff79e08900a5daadb301094a45521214f645849b
Salik Ali <73484829+SalikTheCreator@users.noreply.github.com>
2021-02-22 14:13:48 -0800

    Update Study.js

--- src/Layout/Study.js
+++ src/Layout/Study.js
@@ -1,34 +1,42 @@
 import React, { useEffect, useState } from "react";
-import { readDeck } from "../utils/api/index";
-import { Link, useHistory, useParams } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Link, 
+  useHistory,
+  useParams, 
+} from "react-router-dom";
+
+import { readDeck } from "../utils/api/index"
 
 function Study() {
+  const [cardContent, setCardContent] = useState("");
   const { deckId } = useParams();
   const history = useHistory();
-  const [deckName, setDeckName] = useState("");
-  const [cardsList, setCardsList] = useState([]);
   const [currentCard, setCurrentCard] = useState(0);
+  const [deckName, setDeckName] = useState("");
   const [isFlipped, setIsFlipped] = useState(false);
+  const [cardsList, setCardsList] = useState([]);
   const [showNext, setShowNext] = useState(false);
-  const [cardContent, setCardContent] = useState("");
 
   useEffect(() => {
-    const loadDeck = async () => {
-      const deck = await readDeck(deckId);
-      setDeckName(deck.name);
-      setCardsList(deck.cards);
-      if (deck.cards.length > 0) {
-        setCardContent(deck.cards[currentCard].front);
-      }
-    };
-    loadDeck();
-  }, []);
+    // fetch(`http://localhost:5000/decks/${deckId}?_embed=cards`)
+    //   .then((res) => res.json())
+    readDeck(deckId)
+      .then((json) => {
+        setDeckName(json.name);
+        setCardsList(json.cards);
+        if( json.cards.length === 0){
+          return null
+        }
+        setCardContent(json.cards[currentCard].front);
+      });
+  }, [currentCard, deckId]);
 
   useEffect(() => {
     if (cardsList.length > 0) {
       setCardContent(cardsList[currentCard].front);
     }
-  }, [currentCard]);
+  }, [currentCard, cardsList]);
 
   const flipCard = () => {
     if (isFlipped) {
@@ -79,12 +87,15 @@ function Study() {
         <h2 className="card-title">Study: {deckName}</h2>
       </div>
       <div className="card">
-        <h5 className="card-title">{cardsList.length > 0 ? `Card ${currentCard + 1} of ${cardsList.length}` : "Card 1 of 3"}</h5>
         <div className="card-body">
           {cardsList.length > 2 ? (
             <div className="flip-card">
               <div className="flip-card-inner">
                 <div className="flip-card-front">
+                  <h5 className="card-title">
+                    Card {currentCard + 1} of {cardsList.length}
+                 
+                  </h5>
                   <p className="card-text">{cardContent}</p>
                   <div className="d-grid gap-2 d-md-block"></div>
                 </div>
