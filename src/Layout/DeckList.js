195daac35296dc441327e08761dce008100c117f
Salik Ali <73484829+SalikTheCreator@users.noreply.github.com>
2021-02-22 14:11:06 -0800

    Update DeckList.js

--- src/Layout/DeckList.js
+++ src/Layout/DeckList.js
@@ -1,38 +1,75 @@
-import React from "react";
-import { deleteDeck } from "../utils/api/index";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+// import Header from "./Header";
+// import NotFound from "./NotFound";
+import {
+  deleteDeck, listDecks
+
+} from "../utils/api/index";
+import { 
+  Link,
+} from "react-router-dom";
 
 function DeckList(props) {
-  const handleDeleteClick = async () => {
+  const [deckList, setDeckList] = useState([]);
+
+
+  useEffect(loadDecks, []);
+
+  const handleDeleteClick = (deckId) => {
     if (
       window.confirm("Delete this deck?\n\nYou will not be able to recover it.")
     ) {
-      await deleteDeck(props.id);
+      console.log(deckId, "who's id is here??s")
+      deleteDeck(deckId).then(loadDecks);
+      //props.handleDeleteDeck(deckList.id);
+      
     }
   };
+  function loadDecks() {
+    listDecks().then(setDeckList)
+ 
+  }
+
+
+
+  const mappedDecks = deckList.map((deck) => {
 
   return (
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{props.name}</h5>
-        <h5 className="card-title">{props.cards.length} cards</h5>
-        <p className="card-text">{props.description}</p>
-        <Link to={`/decks/${props.id}`} className="btn btn-secondary">
+    <div className="card" key={deck.id}>
+      <div className="card-body" >
+        <h5 className="card-title">{deck.name}</h5>
+        <h5 className="card-title">{deck.cards.length} cards</h5>
+        <p className="card-text">{deck.description}</p>
+        <Link to={`/decks/${deck.id}`} className="btn btn-secondary">
           View
         </Link>
-        <Link to={`/decks/${props.id}/study`} className="btn btn-primary">
+        <Link to={`/decks/${deck.id}/study`} className="btn btn-primary">
           Study
         </Link>
-        <button
-          className="btn btn-danger float-right"
-          title="Delete deck"
-          onClick={handleDeleteClick}
+        <Link
+          to="/"
+          className="btn btn-danger"
+          onClick={() => handleDeleteClick(deck.id)}
         >
           <span className="oi oi-trash" />
-        </button>
+        </Link>
       </div>
     </div>
   );
+});
+
+  return (
+    <>
+      <Link to="/decks/new" className="btn btn-secondary">
+        <span className="oi oi-plus" />
+        {``} Create Deck
+      </Link>
+      <ul>
+        {mappedDecks}
+      </ul>
+    </>
+  )
 }
 
 export default DeckList;
+  
